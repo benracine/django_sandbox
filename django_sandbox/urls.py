ad6f03a9d834bd84e388f2e5e94b06f6390a0a59
Ben Racine <i3enhamin@gmail.com>
2011-11-12 15:01:24 -0800

    blah

--- django_sandbox/urls.py
+++ django_sandbox/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     # Example:
-    (r'^helloworld/', include('basic.urls')),
+    (r'^/', include('basic.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
